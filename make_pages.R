33667cceaa99a1109ac291879c700106d6f78a97
Ben Best <ben@ecoquants.com>
2023-04-27 12:48:42 +0000

    init FERC doc excerpts (w/out tags; search off)

---
+++ make_pages.R
@@ -0,0 +1,15 @@
+librarian::shelf(
+  glue, here, stringr)
+dir_repos   <- "/share/github"
+dir_scripts <- glue("{dir_repos}/apps_dev/scripts")
+source(glue("{dir_scripts}/common.R"))
+source(glue("{dir_scripts}/db.R"))
+
+rowids <- tbl(con, "ferc_docs") |>
+  pull(rowid) |>
+  sort()
+for (i in rowids){ # i = 1
+  message(glue("i: {i}"))
+  cmd <- glue("quarto render ferc_template.qmd -P id:{i} -o ferc_{str_pad(i, 6, pad = '0')}.html")
+  system(cmd) # i=1: 9:25pm # i=
+}
